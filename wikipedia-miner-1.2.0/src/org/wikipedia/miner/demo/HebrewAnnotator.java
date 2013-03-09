097b5887ca3d998c3a819d99af039508fc2adf9e
Itay Mangshe <itayman@ubuntu.(none)>
2013-03-09 04:41:30 +0200

    Added HebrewAnnotator and changed build.xml and he,xml to support it

--- wikipedia-miner-1.2.0/src/org/wikipedia/miner/demo/HebrewAnnotator.java
+++ wikipedia-miner-1.2.0/src/org/wikipedia/miner/demo/HebrewAnnotator.java
@@ -1,7 +1,8 @@
-package org.wikipedia.miner.demo;
+package org.wikipedia.miner.demo; 
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -14,21 +15,24 @@ import org.wikipedia.miner.annotation.preprocessing.DocumentPreprocessor;
 import org.wikipedia.miner.annotation.preprocessing.PreprocessedDocument;
 import org.wikipedia.miner.annotation.preprocessing.WikiPreprocessor;
 import org.wikipedia.miner.annotation.tagging.DocumentTagger;
-import org.wikipedia.miner.annotation.tagging.DocumentTagger.RepeatMode;
 import org.wikipedia.miner.annotation.tagging.WikiTagger;
+import org.wikipedia.miner.annotation.tagging.DocumentTagger.RepeatMode;
 import org.wikipedia.miner.annotation.weighting.LinkDetector;
+import org.wikipedia.miner.comparison.ArticleComparer;
+import org.wikipedia.miner.db.WDatabase.DatabaseType;
 import org.wikipedia.miner.model.Wikipedia;
 import org.wikipedia.miner.util.WikipediaConfiguration;
 
+
 public class HebrewAnnotator {
 
-	DecimalFormat _df = new DecimalFormat("#0%") ;
-	DocumentPreprocessor _preprocessor ;
+	DocumentPreprocessor _preprocessor ; 
 	Disambiguator _disambiguator ;
 	TopicDetector _topicDetector ;
 	LinkDetector _linkDetector ;
 	DocumentTagger _tagger ;
 
+	DecimalFormat _df = new DecimalFormat("#0%") ;
 
 	public HebrewAnnotator(Wikipedia wikipedia) throws Exception {
 		_preprocessor = new WikiPreprocessor(wikipedia) ;
@@ -39,39 +43,55 @@ public class HebrewAnnotator {
 	}
 
 	public void annotate(String originalMarkup) throws Exception {
-//		PreprocessedDocument doc = _preprocessor.preprocess(originalMarkup) ;
-//		Collection<Topic> allTopics = _topicDetector.getTopics(doc, null) ;
-//		System.out.println("\nAll detected topics:") ;
-//		for (Topic t:allTopics)
-//			System.out.println(" - " + t.getTitle()) ;
-//		ArrayList<Topic> bestTopics = _linkDetector.getBestTopics(allTopics, 0.5) ;
-//		System.out.println("\nTopics that are probably good links:") ;
-//		for (Topic t:bestTopics)
-//			System.out.println(" - " + t.getTitle() + "[" + _df.format(t.getWeight()) + "]" ) ;
-//		String newMarkup = _tagger.tag(doc, bestTopics, RepeatMode.ALL) ;
-//		System.out.println("\nAugmented markup:\n" + newMarkup + "\n") ;
-		System.out.print("bla");
+
+		PreprocessedDocument doc = _preprocessor.preprocess(originalMarkup) ;
+
+		Collection<Topic> allTopics = _topicDetector.getTopics(doc, null) ;
+		System.out.println("\nAll detected topics:") ;
+		for (Topic t:allTopics)
+			System.out.println(" - " + t.getTitle()) ;
+
+		ArrayList<Topic> bestTopics = _linkDetector.getBestTopics(allTopics, 0.5) ;
+		System.out.println("\nTopics that are probably good links:") ;
+		for (Topic t:bestTopics)
+			System.out.println(" - " + t.getTitle() + "[" + _df.format(t.getWeight()) + "]" ) ;
+
+		String newMarkup = _tagger.tag(doc, bestTopics, RepeatMode.ALL) ;
+		System.out.println("\nAugmented markup:\n" + newMarkup + "\n") ;
+
 	}
 
+	public static void main (String[] args) throws Exception {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String args[]) throws Exception {
-
-		WikipediaConfiguration conf = new WikipediaConfiguration(new File(args[0])) ;
-		Wikipedia wikipedia = new Wikipedia(conf, false) ;
-
-		HebrewAnnotator annotator = new HebrewAnnotator(wikipedia) ;
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)) ;
 		while (true) {
-			System.out.println("Enter snippet to annotate (or ENTER to quit):") ;
-			String line = reader.readLine();
-
-			if (line.trim().length() == 0)
-				break ;
-
-			annotator.annotate(line) ;
-
+			System.out.println("Input a string: ");
+			String line = input.readLine() ;
+			System.out.println(line);
 		}
 	}
-}
 
+
+
+
+
+	//		WikipediaConfiguration conf = new WikipediaConfiguration(new File(args[0])) ;
+	//		conf.clearDatabasesToCache() ;
+	//		
+	//		Wikipedia wikipedia = new Wikipedia(conf, false) ;
+	//		
+	//		HebrewAnnotator annotator = new HebrewAnnotator(wikipedia) ;
+	//		
+	//		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8")) ;
+	//
+	//		while (true) {
+	//			System.out.println("Enter snippet to annotate (or ENTER to quit):") ;
+	//			String line = reader.readLine();
+	//			System.out.println("bla");
+	//			if (line.trim().length() == 0)
+	//				break ;
+	//			
+	//			annotator.annotate(line) ;
+	//		}
+	//	}
+}
